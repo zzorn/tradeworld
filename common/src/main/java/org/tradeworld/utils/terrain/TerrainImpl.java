ebe225e4d99fd43f60e1b6c213226b6a6df67ee8
zzorn <zzorn@iki.fi>
2013-03-20 16:40:45 +0200

    Just a stub.

--- common/src/main/java/org/tradeworld/utils/terrain/TerrainImpl.java
+++ common/src/main/java/org/tradeworld/utils/terrain/TerrainImpl.java
@@ -1,5 +1,6 @@
 package org.tradeworld.utils.terrain;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,5 +12,21 @@ public class TerrainImpl implements Terrain {
     // An optimization would be to use some kind of RTree here.  Needs to be thread safe thou.
 
 
+    @Override
+    public Collection<String> getChannelNames() {
+        // TODO
+        return null;
+    }
 
+    @Override
+    public double getValueAt(String channel, double x, double y, double radius) {
+        // TODO
+        return 0;
+    }
+
+    @Override
+    public void getValues(String channel, BoundingBox region, DoubleRaster targetRaster) {
+        // TODO
+
+    }
 }
